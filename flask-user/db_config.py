e9812a161097aaf7fed988cbf8919a3a226e9a79
Munish Sharma <munish.sharma@coda.global>
2020-02-06 15:12:06 +0530

    Update db_config.py

--- flask-user/db_config.py
+++ flask-user/db_config.py
@@ -4,8 +4,8 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
  
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'hudZrh49l0'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'QQdwj3GfQH'
-app.config['MYSQL_DATABASE_DB'] = 'hudZrh49l0'
-app.config['MYSQL_DATABASE_HOST'] = 'remotemysql.com'
+app.config['MYSQL_DATABASE_USER'] = '**********'
+app.config['MYSQL_DATABASE_PASSWORD'] = '**********'
+app.config['MYSQL_DATABASE_DB'] = '<database_name>'
+app.config['MYSQL_DATABASE_HOST'] = '<host_name>'
 mysql.init_app(app)
